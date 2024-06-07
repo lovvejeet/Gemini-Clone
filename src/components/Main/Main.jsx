2bf9c545a895b62553718d1c8014260ec721970e
lovvejeet <135307361+lovvejeet@users.noreply.github.com>
2024-06-07 20:30:21 +0530

    added main page cards text

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,6 +7,32 @@ const Main = () => {
         <p>Gemini</p>
         <img src={assets.user_icon} alt="user_icon" />
       </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello, Dev.</span>
+          </p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="compass_icon" />
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept: urban planning</p>
+            <img src={assets.bulb_icon} alt="compass_icon" />
+          </div>
+          <div className="card">
+            <p>Brianstrom team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="compass_icon" />
+          </div>
+          <div className="card">
+            <p>Improve the readability of the following code</p>
+            <img src={assets.code_icon} alt="compass_icon" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
