799cfa4a0bd9c11f2fb050eca1566ca81fd8a0a2
lovvejeet <135307361+lovvejeet@users.noreply.github.com>
2024-06-07 17:20:53 +0530

    added side bar outline

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Sidebar from "./components/Sidebar/Sidebar";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
 };
 
 export default App;
