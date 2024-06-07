e518482bd7b368cc01ee916152b6e787df2650f1
lovvejeet <135307361+lovvejeet@users.noreply.github.com>
2024-06-07 20:13:30 +0530

    added sidebar collapse and fonts

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,34 +1,44 @@
 import "./sidebar.css";
 import { assets } from "../../assets/assets";
+import { useState } from "react";
 const Sidebar = () => {
+  const [extented, setExtented] = useState(false);
   return (
     <div className="sidebar">
       <div className="top">
-        <img className="menu" src={assets.menu_icon} alt="menu-icon" />
+        <img
+          className="menu"
+          src={assets.menu_icon}
+          alt="menu-icon"
+          onClick={() => setExtented((prev) => !prev)}
+        />
         <div className="new-chat">
           <img src={assets.plus_icon} alt="plus-icon" />
-          <p>New Chat</p>
+          {extented ? <p>New Chat</p> : null}
         </div>
-        <div className="recent">
-          <p className="recent-title">Recent</p>
-          <div className="recent-entry">
-            <img src={assets.message_icon} alt="message_icon" />
-            <p>What is react ...</p>
+
+        {extented ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+              <img src={assets.message_icon} alt="message_icon" />
+              <p>What is react ...</p>
+            </div>
           </div>
-        </div>
+        ) : null}
       </div>
       <div className="bottom">
         <div className="bottom-item recent-entry">
           <img src={assets.question_icon} alt="question_icon" />
-          <p>Help</p>
+          {extented ? <p>Help</p> : null}
         </div>
         <div className="bottom-item recent-entry">
           <img src={assets.history_icon} alt="history_icon" />
-          <p>Activity</p>
+          {extented ? <p>Activity</p> : null}
         </div>
         <div className="bottom-item recent-entry">
           <img src={assets.setting_icon} alt="setting_icon" />
-          <p>Settings</p>
+          {extented ? <p>Settings</p> : null}
         </div>
       </div>
     </div>
