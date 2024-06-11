5410248cfeac9ef07e05ac717aa7277e7a0de147
lovvejeet <135307361+lovvejeet@users.noreply.github.com>
2024-06-11 14:08:15 +0530

    fixed displaying data in main page

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,45 +1,52 @@
-import { createContext, useState } from "react";
+import React, { createContext, useState } from "react";
 import runChat from "../config/gemini";
 
 export const Context = createContext();
 
-const ContextProvider = (props) => {
+export default function ContextProvider(props) {
   const [input, setInput] = useState("");
   const [recentPrompt, setRecentPrompt] = useState("");
   const [prevPrompt, setPrevPrompt] = useState([]);
   const [showResult, setShowResult] = useState(false);
   const [loading, setLoading] = useState(false);
   const [resultDate, setResultDate] = useState("");
+
   const delayPara = (index, nextWord) => {
-    setTimeout(function () {
+    setTimeout(() => {
       setResultDate((prev) => prev + nextWord);
     }, 75 * index);
   };
+
   const onSent = async (prompt) => {
     setResultDate("");
     setLoading(true);
     setShowResult(true);
     setRecentPrompt(input);
-    setPrevPrompt(prev=>[...prev,input])
+    setPrevPrompt((prev) => [...prev, input]);
+
     const response = await runChat(input);
     let responseArray = response.split("**");
-    let newResponse;
+    let newResponse = "";
+
     for (let i = 0; i < responseArray.length; i++) {
-      if (i == 0 || i % 2 !== 1) {
+      if (i === 0 || i % 2 !== 1) {
         newResponse += responseArray[i];
       } else {
         newResponse += "<b>" + responseArray[i] + "</b>";
       }
     }
+
     let newResponse2 = newResponse.split("*").join("</br>");
     let newResponseArray = newResponse2.split(" ");
     for (let i = 0; i < newResponseArray.length; i++) {
       const nextWord = newResponseArray[i];
       delayPara(i, nextWord + " ");
     }
+
     setLoading(false);
     setInput("");
   };
+
   const contextValue = {
     prevPrompt,
     setPrevPrompt,
@@ -52,9 +59,8 @@ const ContextProvider = (props) => {
     input,
     setInput,
   };
+
   return (
-    // eslint-disable-next-line react/prop-types
     <Context.Provider value={contextValue}>{props.children}</Context.Provider>
   );
-};
-export default ContextProvider;
+}
