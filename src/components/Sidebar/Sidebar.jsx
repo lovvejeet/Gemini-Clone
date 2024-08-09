c680bad7bdd78b58ed95c15c714577c729cf4843
lovvejeet <lovejeetsandhu8194@gmail.com>
2024-08-09 16:45:55 +0530

    fixed import file mismatch error

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import "../Sidebar/sidebar.css";
+import "./Sidebar.css";
 import { assets } from "../../assets/assets";
 import { useContext, useState } from "react";
 import { Context } from "../../context/Context";
